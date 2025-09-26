ddd8923460eaee9eeb5bbed3eceb7bd35d17f3c0
satriyaadhip <satriya.pradana1702.sp@gmail.com>
2025-09-26 21:04:58 +0700

    Dashboard (static) sudah jadii yeayy

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function() {
-    return view('home');
+Route::get('/dashboard', function() {
+    return view('dashboard');
 });
 
 Route::get('/about', function() {
